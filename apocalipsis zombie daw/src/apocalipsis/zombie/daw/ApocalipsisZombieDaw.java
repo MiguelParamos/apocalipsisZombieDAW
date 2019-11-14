76613436852f6fd32a62e7047e349ba9bf2fceb6
pedrodnc <pedropablocampeon@gmail.com>
2019-11-14 17:59:07 +0100

    Update ApocalipsisZombieDaw.java

--- apocalipsis zombie daw/src/apocalipsis/zombie/daw/ApocalipsisZombieDaw.java
+++ apocalipsis zombie daw/src/apocalipsis/zombie/daw/ApocalipsisZombieDaw.java
@@ -7,7 +7,9 @@ package apocalipsis.zombie.daw;
 import alvaro.FuncionesA;
 /**
  *
- * @author cenec
+ * @author pedro issac
+ * @version 0.1
+ * @since 0.1
  */
 public class ApocalipsisZombieDaw {
 
