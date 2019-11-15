96b8fdbcd2fd3d6d7593399993e98202a8b29808
ssbriales <57761424+ssbriales@users.noreply.github.com>
2019-11-15 15:38:15 +0100

    comentario de clases, funciones y variables

--- apocalipsis zombie daw/src/Noelia/Silvia/FuncionesNS.java
+++ apocalipsis zombie daw/src/Noelia/Silvia/FuncionesNS.java
@@ -3,18 +3,21 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Noelia.Silvia;
+package noelia.silvia;
 
 /**
- *
- * @author nrcif
+ *clase del personaje Noelia y Silvia
+ *@author Noelia
+ *@author Silvia
+ *@verison 0.1
+ *@since 0.1
  */
 public class FuncionesNS {
-    public static String nombre; // Nombre del personaje
-    public static byte velocidad;
-    public static byte fuerza;
-    public static byte intuicion;
-    public static byte percepcion;
+    public static String nombre; //Nombre del personaje
+    public static byte velocidad; //Velocidad del personaje entre 0 y 100
+    public static byte fuerza; //Fuerza del personaje entre 0 y 100
+    public static byte intuicion; //Intuición del personaje entre 0 y 100
+    public static byte percepcion; //Percepción del personaje entre 0 y 100
     
     
 }
