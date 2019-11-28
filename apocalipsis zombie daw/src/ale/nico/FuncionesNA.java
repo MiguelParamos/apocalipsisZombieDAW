726f418f69cdda0a767ddd767ca6a370c2099adf
XXnikoDarkXX <nicocheto1@gmail.com>
2019-11-28 16:02:55 +0100

    punteriaPuig

--- apocalipsis zombie daw/src/ale/nico/FuncionesNA.java
+++ apocalipsis zombie daw/src/ale/nico/FuncionesNA.java
@@ -21,7 +21,8 @@ public class FuncionesNA {
     public static byte intuicion; //intuicion del personaje entre 0 y 100
     public static byte percepcion;//intuicion del personaje entre 0 y 100
     public static byte vida;//vida que tiene nuestro personaje (100)
-    public static boolean  comprobacionInicial (byte velocidad,byte fuerza, byte intuicion, byte percepcion){
+    public static byte punteria; //Puntería que tiene nuestro personaje entre 0 y 100
+    public static boolean  comprobacionInicial (byte velocidad,byte fuerza, byte intuicion, byte percepcion, byte punteria){
         if (velocidad+fuerza+intuicion+percepcion==200) {
             return true;
     }else
