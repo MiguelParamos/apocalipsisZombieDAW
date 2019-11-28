498a6d1d8c6c59b2c0365f3a81f638fd7edf6d86
Kenesthor <48281757+Kenesthor@users.noreply.github.com>
2019-11-28 15:34:55 +0100

    Cambio en FuncionesA
    Turno 8

--- apocalipsis zombie daw/src/alvaro/FuncionesA.java
+++ apocalipsis zombie daw/src/alvaro/FuncionesA.java
@@ -45,10 +45,42 @@ public class FuncionesA {
                 return("Ayudo a Natalia a levantarse.");
             }else if(turno==7&&accion.equals("miro el mensaje")){
                 return("Ahora que tengo las manos libres, cojo el teléfono que estaba sonando.");
+            }else if(turno==8&&accion.equals("miro el mensaje")){
+                return("Ahora que tengo las manos libres, cojo el teléfono que estaba sonando.");
             }
         return "";
- 
-
+    }
+    
+    public static String dondeLlego (int distancia,String mensaje){
+        
+        if(distancia>=3){
+            switch(mensaje){
+                case "ventana":
+                    return "Estoy en mitad del aula";
+                case "mitad del aula":
+                    return "Estoy en la puerta";
+                case "en la puerta":
+                    return "He bajado las escaleras, estoy en la 1ª planta";
+                case "en el tablon":
+                    return "He bajado las escaleras, estoy en la 1ª planta"; 
+                case "en la puerta hacia el gallinero":
+                    return "Estoy en la puerta del gallinero"; 
+            }
+        }else{
+            switch(mensaje){
+                case "ventana":
+                    return "Voy por la fila 3";
+                case "mitad del aula": 
+                    return "Voy por la ultima fila";
+                case "en la puerta":
+                    return "Voy por mitad de las escaleras";
+                case "en el tablon":
+                    return "Voy por mitad de las escaleras"; 
+                case "en la puerta hacia el gallinero":
+                    return "Estoy en la puerta hacia el gallinero"; 
+            }
+        }
+        return"";
     }
 }
 
